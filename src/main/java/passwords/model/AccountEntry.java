8ec2300861010c2e665bbf91522dd476b18cf0d7
Iwo Dudkiewicz <iwo.dudkiewicz@gmail.com>
2026-01-15 18:05:04 +0100

    Create AccountEntry class

--- src/main/java/passwords/model/AccountEntry.java
+++ src/main/java/passwords/model/AccountEntry.java
@@ -1,4 +1,48 @@
 package passwords.model;
 
 public class AccountEntry {
+    private String platform;
+    private String login;
+    private String password;
+
+    public AccountEntry(String platform, String login, String password) {
+        this.platform = platform;
+        this.login = login;
+        this.password = password;
+    }
+
+    // GETTERS
+    public String getPlatform() {
+        return platform;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // SETTERS
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "AccountEntry{" +
+                "platform='" + platform + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password +
+                '}';
+    }
 }
