556f35c44be6f2ad8c04ce005204bfda53fc3425
Iwo Dudkiewicz <iwo.dudkiewicz@gmail.com>
2026-01-15 18:42:28 +0100

    Create saveToFile() logic

--- src/main/java/passwords/Main.java
+++ src/main/java/passwords/Main.java
@@ -1,13 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+package passwords;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import passwords.logic.PasswordManager;
+import passwords.model.AccountEntry;
+
+public class Main {
+    public static void main(String[] args) {        // 1. Tworzymy menedżera
+        PasswordManager manager = new PasswordManager();
+
+        manager.addEntry(new AccountEntry("Google", "mymail@gmail.com", "GooglePassword123!"));
+        manager.addEntry(new AccountEntry("Facebook", "FBUser", "FacebookPassword123"));
+
+        manager.saveToFile();
+
+        System.out.println("Loading finished. Check passwords.json file in directory of the projeckt.");
     }
 }
--- src/main/java/passwords/logic/PasswordManager.java
+++ src/main/java/passwords/logic/PasswordManager.java
@@ -1,4 +1,33 @@
 package passwords.logic;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import passwords.model.AccountEntry;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PasswordManager {
+    private List<AccountEntry> entries = new ArrayList<>();
+    private final String FILE_NAME = "passwords.json";
+
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public void addEntry(AccountEntry entry) {
+        entries.add(entry);
+    }
+
+    public List<AccountEntry> getAllEntries() {
+        return entries;
+    }
+
+    public void saveToFile() {
+        try (FileWriter writer = new FileWriter(FILE_NAME)) {
+            gson.toJson(entries, writer);
+            System.out.println("Data successfully saved to " + FILE_NAME);
+        } catch (IOException e) {
+            System.err.println("Error while saving: " + e.getMessage());
+        }
+    }
 }
